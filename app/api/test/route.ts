38fad5a919076cfb90160ef6f76fa8a047f4bb05
AbhishekCS3459 <abhishekverman3459@gmail.com>
2024-11-15 13:02:41 +0530

    changed the handler

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,12 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
- 
-type ResponseData = {
-  message: string
-}
- 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
-}
+export async function GET() {
+    const res = await fetch('https://api.vercel.app/blog');
+    const data = await res.json();
+    return Response.json(data);
+  }
