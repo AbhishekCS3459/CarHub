0024a9fc83a96e0da01a61c8dc0964d7cb280588
AbhishekCS3459 <abhishekverman3459@gmail.com>
2024-11-15 13:06:12 +0530

    changed adeed more route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,23 @@
+import {  NextResponse } from "next/server";
+
+//req is short for request
 export async function GET() {
-    const res = await fetch('https://api.vercel.app/blog');
-    const data = await res.json();
-    return Response.json(data);
-  }
+  return NextResponse.json(
+    { message: "this is a get request" },
+    { status: 200 }
+  );
+}
+
+export async function POST() {
+  return NextResponse.json(
+    { message: "This is a post request" },
+    { status: 200 }
+  );
+}
+
+export async function PATCH() {
+  return NextResponse.json(
+    { message: "This is a patch request" },
+    { status: 200 }
+  );
+}
